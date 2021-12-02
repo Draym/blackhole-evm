1cde7b748b19e294e4ebd1acf3cc098b8b60b888
Draym <kevin.draym@gmail.com>
2021-12-02 15:28:35 +0800

    setup BattleLogic

---
+++ contracts/game/BattleLogic.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+
+import "../character/NokaiStats.sol";
+
+// SPDX-License-Identifier: MIT
+contract BattleLogic {
+
+    function battle(NokaiStats.Profile memory attacker, NokaiStats.Profile memory defender) external pure returns (uint256, uint256, uint256) {
+        uint256 attackerHp = attacker.currentHp;
+        uint256 defenderHp = defender.currentHp;
+
+        uint256 turns = 0;
+
+        while (attackerHp > 0 || defenderHp > 0) {
+
+        }
+
+        return (turns, attackerHp >= 0 ? attackerHp : 0, defenderHp >= 0 ? defenderHp : 0);
+    }
+}
