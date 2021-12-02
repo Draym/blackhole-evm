313a880860f12eb5012f2602f1bc8445b94341d2
Draym <kevin.draym@gmail.com>
2021-12-02 15:36:40 +0800

    save arena

---
+++ contracts/game/Arena.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+/**
+ * Arena mode PVP, 1v1 with up to 3 or 4 Nokai per player. The Techniques should be adjusted for team game with effect on row / columns instead.
+ * Can be implemented for a future release
+ */
+// SPDX-License-Identifier: MIT
+contract Arena {
+}
