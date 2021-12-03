516f48f81bc3f3c53324ee0da2976fa8b76b90d0
Draym <kevin.draym@gmail.com>
2021-12-03 10:17:52 +0800

    setup technique stats externally

--- contracts/character/NokaiTechnique.sol
+++ contracts/character/NokaiTechnique.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.0;
 contract NokaiTechnique {
 
     enum Technique {
-        DOUBLE_HIT, // hit twice at 75%
-        GUARDIAN, // block 50% dmg after defense
-        GLUTTON, // heal for 30% dmg dealt
-        TOXIC, // poison for 10% dmg dealt, stack infinite
-        DESTROYER, // break 50% defense
-        FOOL, // cancel opponent passives
-        WEAK_POINT, // critical hit - deal +30% dmg
+        SPECIALIST, // hit twice at 75%
+        GUARDIAN, // block 40% dmg after defense
+        GLUTTON, // steal 30% opponent's attack stat
+        BUFFOON, // reduce opponent's attack and defence by 30%
+        DESTROYER, // break 50% opponent's defense
+        FOOL, // cancel opponent's passives
+        DEADLY, // critical hit - deal +40% dmg
         SPEEDY, // always attack first
         REVENGER, // hit twice at 100% on death
         MIMIC // copy opponent technique
@@ -18,5 +18,27 @@ contract NokaiTechnique {
 
     function get(uint256 _newNokaiId) external view returns (Technique) {
         return Technique(uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, _newNokaiId))) % 10);
+    }
+
+    function specialist() external pure returns (uint256) {
+        return 75;
+    }
+    function guardian() external pure returns (uint256) {
+        return 40;
+    }
+    function glutton() external pure returns (uint256) {
+        return 30;
+    }
+    function buffoon() external pure returns (uint256) {
+        return 30;
+    }
+    function destroyer() external pure returns (uint256) {
+        return 50;
+    }
+    function deadly() external pure returns (uint256) {
+        return 40;
+    }
+    function revenger() external pure returns (uint256) {
+        return 100;
     }
 }
