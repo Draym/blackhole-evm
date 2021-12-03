e9b96831f667537376ce459de39347f8049472bf
Draym <kevin.draym@gmail.com>
2021-12-03 16:20:15 +0800

    clean up role

--- contracts/tokens/Item.sol
+++ contracts/tokens/Item.sol
@@ -5,18 +5,18 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // SPDX-License-Identifier: MIT
 contract Item is ERC20, AccessControl {
-    bytes32 public constant CRAFT_MANAGER_ROLE = keccak256("CRAFT_MANAGER_ROLE");
-    bytes32 public constant INVENTORY_MANAGER_ROLE = keccak256("INVENTORY_MANAGER_ROLE");
+    bytes32 public constant MINT_ROLE = keccak256("MINT_ROLE");
+    bytes32 public constant BURN_ROLE = keccak256("BURN_ROLE");
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function craft(address user, uint256 amount) external virtual onlyRole(CRAFT_MANAGER_ROLE) {
+    function craft(address user, uint256 amount) external virtual onlyRole(MINT_ROLE) {
         _mint(user, amount);
     }
 
-    function consume(address user, uint256 amount) external virtual onlyRole(INVENTORY_MANAGER_ROLE) {
+    function consume(address user, uint256 amount) external virtual onlyRole(BURN_ROLE) {
         _burn(user, amount);
     }
 }
