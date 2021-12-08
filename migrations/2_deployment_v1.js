bba5b969ee9f8c909c98fe2d1cc9d90189b190b0
Draym <kevin.draym@gmail.com>
2021-12-08 13:57:02 +0800

    update scripts

--- migrations/2_deployment_v1.js
+++ migrations/2_deployment_v1.js
@@ -185,24 +185,4 @@ module.exports = async function (deployer, network, accounts) {
     });
 
     await fs.writeFile(`${path}/result.json`, JSON.stringify(deployed))
-    await fs.copyFile("build/contracts/EnergyShock.json", `${path}/EnergyShock.json`);
-    await fs.copyFile("build/contracts/LifeEssence.json", `${path}/LifeEssence.json`);
-    await fs.copyFile("build/contracts/PotionEssence.json", `${path}/PotionEssence.json`);
-    await fs.copyFile("build/contracts/DarkEnergy.json", `${path}/DarkEnergy.json`);
-    await fs.copyFile("build/contracts/DarkMatter.json", `${path}/DarkMatter.json`);
-    await fs.copyFile("build/contracts/PlasmaEnergy.json", `${path}/PlasmaEnergy.json`);
-    await fs.copyFile("build/contracts/VoidEssence.json", `${path}/VoidEssence.json`);
-    await fs.copyFile("build/contracts/HolyArtefact.json", `${path}/HolyArtefact.json`);
-    await fs.copyFile("build/contracts/HolyCore.json", `${path}/HolyCore.json`);
-    await fs.copyFile("build/contracts/StarterPack.json", `${path}/StarterPack.json`);
-    await fs.copyFile("build/contracts/CraftsmanProfile.json", `${path}/CraftsmanProfile.json`);
-    await fs.copyFile("build/contracts/NokaiTechnique.json", `${path}/NokaiTechnique.json`);
-    await fs.copyFile("build/contracts/NokaiStats.json", `${path}/NokaiStats.json`);
-    await fs.copyFile("build/contracts/Nokai.json", `${path}/Nokai.json`);
-    await fs.copyFile("build/contracts/BattleLogic.json", `${path}/BattleLogic.json`);
-    await fs.copyFile("build/contracts/BlackHole.json", `${path}/BlackHole.json`);
-    await fs.copyFile("build/contracts/CraftManager.json", `${path}/CraftManager.json`);
-    await fs.copyFile("build/contracts/GameManager.json", `${path}/GameManager.json`);
-    await fs.copyFile("build/contracts/Inventory.json", `${path}/Inventory.json`);
-    await fs.copyFile("build/contracts/NokaiGacha.json", `${path}/NokaiGacha.json`);
 };
---
+++ scripts/migrate-deployed-v1.js
@@ -0,0 +1,32 @@
+const fs = require('fs').promises;
+const currentVersion = "v1";
+
+let path = `deployed/${currentVersion}`;
+
+const migrate = async function () {
+    await fs.mkdir(path, {recursive: true}, (err) => {
+    });
+
+    await fs.copyFile("build/contracts/EnergyShock.json", `${path}/EnergyShock.json`);
+    await fs.copyFile("build/contracts/LifeEssence.json", `${path}/LifeEssence.json`);
+    await fs.copyFile("build/contracts/PotionEssence.json", `${path}/PotionEssence.json`);
+    await fs.copyFile("build/contracts/DarkEnergy.json", `${path}/DarkEnergy.json`);
+    await fs.copyFile("build/contracts/DarkMatter.json", `${path}/DarkMatter.json`);
+    await fs.copyFile("build/contracts/PlasmaEnergy.json", `${path}/PlasmaEnergy.json`);
+    await fs.copyFile("build/contracts/VoidEssence.json", `${path}/VoidEssence.json`);
+    await fs.copyFile("build/contracts/HolyArtefact.json", `${path}/HolyArtefact.json`);
+    await fs.copyFile("build/contracts/HolyCore.json", `${path}/HolyCore.json`);
+    await fs.copyFile("build/contracts/StarterPack.json", `${path}/StarterPack.json`);
+    await fs.copyFile("build/contracts/CraftsmanProfile.json", `${path}/CraftsmanProfile.json`);
+    await fs.copyFile("build/contracts/NokaiTechnique.json", `${path}/NokaiTechnique.json`);
+    await fs.copyFile("build/contracts/NokaiStats.json", `${path}/NokaiStats.json`);
+    await fs.copyFile("build/contracts/Nokai.json", `${path}/Nokai.json`);
+    await fs.copyFile("build/contracts/BattleLogic.json", `${path}/BattleLogic.json`);
+    await fs.copyFile("build/contracts/BlackHole.json", `${path}/BlackHole.json`);
+    await fs.copyFile("build/contracts/CraftManager.json", `${path}/CraftManager.json`);
+    await fs.copyFile("build/contracts/GameManager.json", `${path}/GameManager.json`);
+    await fs.copyFile("build/contracts/Inventory.json", `${path}/Inventory.json`);
+    await fs.copyFile("build/contracts/NokaiGacha.json", `${path}/NokaiGacha.json`);
+}
+
+migrate()
